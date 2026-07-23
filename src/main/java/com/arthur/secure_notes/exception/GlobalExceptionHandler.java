c877ae524d959dd27e1126077246461de35c2ffd
Arthur de Lara <arthur.zilli@hotmail.com>
2026-07-23 11:00:14 -0300

    fix: correcao global exception

--- src/main/java/com/arthur/secure_notes/exception/GlobalExceptionHandler.java
+++ src/main/java/com/arthur/secure_notes/exception/GlobalExceptionHandler.java
@@ -1,9 +1,14 @@
 package com.arthur.secure_notes.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -11,6 +16,20 @@ public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     public String tratarCredenciaisInvalidas(CredenciaisInvalidasException exception) {
         return exception.getMessage();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> tratarErrosDeValidacao(
+            MethodArgumentNotValidException exception
+    ) {
+        return exception.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .collect(Collectors.toMap(
+                        FieldError::getField,
+                        FieldError::getDefaultMessage
+                ));
     }
 
 }
